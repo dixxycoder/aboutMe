02decbb5633c1d8ad23fc7b949c81dbb11325b60
Grant Spraker <grant.spraker@gmail.com>
2021-09-24 09:49:58 -0600

    can change the back ground to a blue.

--- aboutMe.sem/ViewController.swift
+++ aboutMe.sem/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Button Corner Radius and Border code from StackOverflow.com
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
     }
 
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
 
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+
+class ViewController: UIViewController
+{
+    // Lable
+    @IBOutlet weak var meLable: UILabel!
+    
+    // Button "tap me"
+    @IBOutlet weak var changeButton: UIButton!
+    
+    // Button "color"
+    @IBOutlet weak var colorButton: UIButton!
+    
+
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    // Change background and lable
+    @IBAction func changeColorb(_ sender: Any)
+    {
+        self.view.backgroundColor = UIColor.blue
+         meLable.text = "I'm Blue?"
+    }
+    
 }
 
